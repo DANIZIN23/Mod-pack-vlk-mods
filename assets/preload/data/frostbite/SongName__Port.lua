ea85f68a2143e590be6d52ac15b87350708e1675
Danibomfin <98532797+Danibomfin@users.noreply.github.com>
2022-08-08 06:48:06 -0300

    Add files via upload

---
+++ assets/preload/data/frostbite/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Recriation by xLegend_123", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
